cbb05c6e5f35106408073d97c279c69d61c144af
evavro <me@madhax.io>
2017-10-07 22:30:30 -0700

    :art: :anchor: HyperRef scaffolding

---
+++ src/ref.js
@@ -0,0 +1,18 @@
+// TODO: incorporate json-where
+
+export class HyperRef {
+
+  constructor (path, core) {
+    this.path = path
+    this.core = core
+  }
+
+  validate () {
+
+  }
+
+  async resolve () {
+
+  }
+
+}
